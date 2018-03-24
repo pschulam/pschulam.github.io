4b1c641693ba721a8d98a3c622018eaead258e74
Peter Schulam <pschulam@gmail.com>
2018-03-24 16:04:01 -0400

    Add a sample post.

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Peter Schulam"  # (translatable)
 BLOG_TITLE = "Peter Schulam"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://pschulam.github.io"
+SITE_URL = "http://pschulam.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://pschulam.com/"
